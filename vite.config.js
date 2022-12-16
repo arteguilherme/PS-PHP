6e322eb732995e595a098756003d7df5837a6807
Guilherme Camilo Almeida <arteguilherme@gmail.com>
2022-12-16 11:22:53 -0400

    Config tailwind css

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            content: [
+                "./resources/**/*.blade.php",
+                "./resources/**/*.js",
+                "./resources/**/*.vue",
+            ],
         }),
     ],
 });
